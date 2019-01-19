0d8ea885a4d19b28316ac03794c45f956fc6addb
Luca Saiu <positron@gnu.org>
2019-01-19 22:13:14 +0100

    WIP implement stack height saving and resetting

--- jitterc/jitterc-generate.c
+++ jitterc/jitterc-generate.c
@@ -1369,6 +1369,9 @@ jitterc_emit_stack_operation_definitions (FILE *f, const struct jitterc_vm *vm)
       jitterc_emit_stack_operation_definition (f, stack, "over", 0);
       jitterc_emit_stack_operation_definition (f, stack, "nip", 0);
 
+      jitterc_emit_stack_operation_definition (f, stack, "height", 0);
+      jitterc_emit_stack_operation_definition (f, stack, "set_height", 1);
+
       jitterc_emit_stack_operation_definition (f, stack, "unary", 1);
       jitterc_emit_stack_operation_definition (f, stack, "binary", 1); // Not a mistake.
     }
