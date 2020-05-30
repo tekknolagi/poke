052a69a08645f22afd339ff52e3fdfb165eef697
Luca Saiu <positron@gnu.org>
2020-05-31 00:02:48 +0200

    add new facility for printing number with human-readable metric or IEC prefixes
    * jitter/jitter-human-prefix.h: New file.
    * jitter/jitter-human-prefix.c: New file.
    
    * Makefile.am (lib_libjitterutility__partial_la_SOURCES): Add new sources
    jitter/jitter-human-prefix.h and jitter/jitter-human-prefix.c.
    (jitterinclude_HEADERS): Install new header jitter/jitter-human-prefix.h.

---
+++ jitter/jitter-human-prefix.c
@@ -0,0 +1,154 @@
+/* Jitter utility: human prefixes.
+
+   Copyright (C) 2020 Luca Saiu
+   Written by Luca Saiu
+
+   This file is part of Jitter.
+
+   Jitter is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Jitter is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Jitter.  If not, see <http://www.gnu.org/licenses/>. */
+
+
+#include <jitter/jitter-human-prefix.h>
+
+#include <stdbool.h>
+#include <stdlib.h>   /* for size_t */
+
+
+/* The description of a metric or IEC prefix including its value and name: for
+   example, {1000, "k"}. */
+struct jitter_human_descriptor
+{
+  /* The value. */
+  double value;
+
+  /* The prefix abbreviated name. */
+  const char *name;
+};
+
+/* An array of decimal prefixes in increasing order. */
+static const struct jitter_human_descriptor
+jitter_human_descriptor_decimal [] =
+  {{1e-24, "y"},
+   {1e-21, "z"},
+   {1e-18, "a"},
+   {1e-15, "f"},
+   {1e-12, "p"},
+   {1e-9,  "n"},
+   {1e-6,  "u"},
+   {1e-3,  "m"},
+   {1,     ""},
+   {1e3,   "k"},
+   {1e6,   "M"},
+   {1e9,   "G"},
+   {1e12,  "T"},
+   {1e15,  "P"},
+   {1e18,  "E"},
+   {1e21,  "Z"},
+   {1e24,  "Y"}};
+/* How many elements there are in jitter_human_descriptor. */
+static const size_t jitter_human_descriptor_decimal_no
+= (sizeof (jitter_human_descriptor_decimal)
+   / sizeof (struct jitter_human_descriptor));
+
+/* Like jitter_human_descriptor and jitter_human_descriptor_decimal_small_no ,
+   but for binary prefixes. */
+static const struct jitter_human_descriptor
+jitter_human_descriptor_binary [] =
+  {{1 / 1024. / 1024. / 1024. / 1024. / 1024. / 1024. / 1024. / 1024., "yi"},
+   {1 / 1024. / 1024. / 1024. / 1024. / 1024. / 1024. / 1024.,         "zi"},
+   {1 / 1024. / 1024. / 1024. / 1024. / 1024. / 1024.,                 "ai"},
+   {1 / 1024. / 1024. / 1024. / 1024. / 1024.,                         "fi"},
+   {1 / 1024. / 1024. / 1024. / 1024.,                                 "pi"},
+   {1 / 1024. / 1024. / 1024.,                                         "ni"},
+   {1 / 1024. / 1024.,                                                 "ui"},
+   {1 / 1024.,                                                         "mi"},
+   {1,                                                                 ""},
+   {1024.,                                                             "ki"},
+   {1024. * 1024.,                                                     "Mi"},
+   {1024. * 1024. * 1024.,                                             "Gi"},
+   {1024. * 1024. * 1024. * 1024.,                                     "Ti"},
+   {1024. * 1024. * 1024. * 1024. * 1024.,                             "Pi"},
+   {1024. * 1024. * 1024. * 1024. * 1024. * 1024.,                     "Ei"},
+   {1024. * 1024. * 1024. * 1024. * 1024. * 1024. * 1024.,             "Zi"},
+   {1024. * 1024. * 1024. * 1024. * 1024. * 1024. * 1024. * 1024.,     "Yi"}};
+static const size_t jitter_human_descriptor_binary_no
+= (sizeof (jitter_human_descriptor_binary)
+   / sizeof (struct jitter_human_descriptor));
+
+/* A helper function for jitter_human_readable.  This assumes that in is
+   strictly positive, and searches for the appropriate unit by scanning the
+   pointed array of descriptors in order. */
+static void
+jitter_human_readable_with (const struct jitter_human_descriptor *descriptors,
+                               size_t descriptor_no,
+                               double *out, const char **prefix, double in)
+{
+  /* Handle empty descriptor tables. */
+  if (descriptor_no == 0)
+    {
+      * out = in;
+      * prefix = "";
+    }
+  /* From now on we can be sure that there is at least one descriptor. */
+
+  bool up = in < 1;
+  
+  /* Search for the most appropriate unit. */
+  const struct jitter_human_descriptor *best;
+  int delta = up ? 1 : -1;
+  const struct jitter_human_descriptor *initial
+    = up ? descriptors : descriptors + descriptor_no - 1;
+  const struct jitter_human_descriptor *limit = initial + delta * descriptor_no;
+  for (best = initial; best != limit; best += delta)
+    if (up && in <= best->value
+        || ! up && in >= best->value)
+        break;
+
+  /* Use the descriptor we chose. */
+  * out = in / best->value;
+  * prefix = best->name;  
+}
+
+void
+jitter_human_readable (double *out, const char **scale,
+                          double in,
+                          bool binary)
+{
+  const struct jitter_human_descriptor *descriptors;
+  size_t descriptors_no;
+  /* Zero or never require any prefixes. */
+  if (in == 0)
+    {
+      * out = 0;
+      * scale = "";
+    }
+  /* I do not want to depend on the math library just for the abs function.
+     Handle the negative case with a trivial recursive call. */
+  else if (in < 0)
+    {
+      jitter_human_readable (out, scale, - in, binary);
+      * out = - * out;
+      return;
+    }
+  
+  /* If we arrived here in is non-negative. */
+  if (binary)
+    jitter_human_readable_with (jitter_human_descriptor_binary,
+                                   jitter_human_descriptor_binary_no,
+                                   out, scale, in);
+  else /* ! binary */
+    jitter_human_readable_with (jitter_human_descriptor_decimal,
+                                   jitter_human_descriptor_decimal_no,
+                                   out, scale, in);
+}
---
+++ jitter/jitter-human-prefix.h
@@ -0,0 +1,88 @@
+/* Jitter utility: human prefix header.
+
+   Copyright (C) 2020 Luca Saiu
+   Written by Luca Saiu
+
+   This file is part of Jitter.
+
+   Jitter is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Jitter is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Jitter.  If not, see <http://www.gnu.org/licenses/>. */
+
+
+#ifndef JITTER_HUMAN_PREFIX_H_
+#define JITTER_HUMAN_PREFIX_H_
+
+#include <stdbool.h>
+
+
+/* Introduction.
+ * ************************************************************************** */
+
+/* This is a convenience facility for printing floating-point numbers with
+   metric (decimal-based) or IEC (binary-based) prefixes, for human-readable
+   output. */
+
+
+
+
+/* Function API.
+ * ************************************************************************** */
+
+/* Find the best human-readable prefix for the number in.  Write the value of in
+   appropriately scaled to the location pointed by out, and write the
+   abbreviation for the prefix to the location pointed by scale.  Use metric
+   (decimal-based) prefixes if binary is false, and IEC (binary-based) prefixes
+   if binary is true. */
+void
+jitter_human_readable (double *out, const char **scale,
+                       double in,
+                       bool binary)
+  __attribute__ ((nonnull (1), nonnull (2)));
+
+
+
+
+/* Convenience macro API.
+ * ************************************************************************** */
+
+/* This is a convenient way of using jitter_human_readable without cluttering
+   the code with explicit variable declarations.
+   Expand to a sequence of two variable declarations and a statements, without
+   any nested blocks.
+   In the expansion:
+   - an automatic variable is declared, with type double and name
+     _jitter_value_name;
+   - another automatic variable is declared, named like _jitter_value_name with
+     the suffix _prefix -- for example, if _jitter_value_name is given as
+     total_size, then the second automatic variable in the expansion is
+     named total_size_prefix
+   - jitter_human_readable is called, supplying pointers to the two 
+     automatic variables, the given value expression, and the the given
+     binary expression.  This initializes the two automatic variables.
+   The following code in the same C block is supposed to use the two automatic
+   variables, typically for printing.
+   Example:
+     JITTER_HUMAN_READABLE (size, blocks * block_size_in_bytes, true);
+     printf ("size: %.2f%sB\n", size, size_prefix); */
+#define JITTER_HUMAN_READABLE_(_jitter_value_name,              \
+                               _jitter_value_exp,               \
+                               _jitter_binary)                  \
+  double _jitter_value_name;                                    \
+  const char *_jitter_value_name ## _prefix;                    \
+  jitter_human_readable (& (_jitter_value_name),                \
+                            & (_jitter_value_name ## _prefix),  \
+                            (_jitter_value_exp),                \
+                            (_jitter_binary))
+
+
+#endif // #ifndef JITTER_HUMAN_PREFIX_H_
