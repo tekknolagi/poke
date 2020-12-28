55cdab0ff39a23df4594226e445f6d71ca4ca42a
Luca Saiu <positron@gnu.org>
2020-12-28 12:10:51 +0100

    profiling instrumentation ordering bug fix
    * jitterc/jitterc-generate.c
    (jitterc_emit_executor_ordinary_specialized_instructions) <profile
    instrumentation>: Check the definition of either one macro of the other in the
    preprocessor conditional, instead of the same one twice.

--- jitterc/jitterc-generate.c
+++ jitterc/jitterc-generate.c
@@ -2730,7 +2730,7 @@ jitterc_emit_executor_ordinary_specialized_instructions
       EMIT("     (VMPREFIX_OWN_SPECIAL_PURPOSE_STATE_DATA,\n");
       EMIT("      JITTER_SPECIALIZED_INSTRUCTION_OPCODE);\n");
       EMIT("#endif\n");
-      EMIT("#if defined (JITTER_PROFILE_SAMPLE) || defined (JITTER_PROFILE_SAMPLE)\n");
+      EMIT("#if defined (JITTER_PROFILE_COUNT) || defined (JITTER_PROFILE_SAMPLE)\n");
       EMIT("  /* Force the compiler not move profiling instrumentation code\n");
       EMIT("     beyond this point; this way the actual user code is timed.\n");
       EMIT("     This is still not perfect, as residuals are materialised before\n");
